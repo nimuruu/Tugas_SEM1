7dd8e127a0380c21936eaf09c9f978e25dca3aca
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-02 21:22:44 +0700

    Update Feature

---
+++ src/VolumeKubus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class VolumeKubus {
+
+    public static void main(String[] args) {
+        double sisi, volume;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukan panjang sisi kubus : ");
+        sisi = scanner.nextDouble();
+
+        volume = sisi * sisi * sisi;
+
+        System.out.print("Volume kubusnya adalah : " + volume);
+
+        scanner.close();
+
+    }
+}
