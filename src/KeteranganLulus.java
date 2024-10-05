d4caa1803b793e6b4af2968358b45291374350a1
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-05 20:08:17 +0700

    Tugas Pertemuan 2

---
+++ src/KeteranganLulus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class KeteranganLulus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukan nilai murid : ");
+        int nilai = scanner.nextInt();
+
+        if (nilai >= 70) {
+            System.out.print("Nilai : " + nilai + ", Maka murid LULUS");
+        }
+
+        else {
+            System.out.print("Nilai : " + nilai + ", Maka murid TIDAK LULUS");
+        }
+
+        scanner.close();
+
+
+    }
+
+
+}
