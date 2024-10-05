abe6437cc896a34418d5e0b4a902a366afb84c75
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-05 19:21:45 +0700

    Tugas Pertemuan 2

---
+++ src/GanjilGenap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GanjilGenap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukan angka : ");
+        int angka = scanner.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.print(angka + " adalah bilangan Genap");
+        }
+
+        else {
+            System.out.print(angka + " adalah bilangan Ganjil");
+        }
+
+        scanner.close();
+
+    }
+}
