d4c8bfd558d57dff1fc19fd5e11802b82d7a2383
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-06 09:07:25 +0700

    Update tugas

---
+++ src/KonversiNilai.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class KonversiNilai {
+    public static void main(String[] args) {
+        String ABC;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukan nilai :");
+        int nilai = scanner.nextInt();
+
+        if (nilai >= 85){
+            ABC = "A";
+        }   else if (nilai >= 75){
+            ABC = "B";
+        }   else if (nilai >= 65){
+            ABC = "C";
+        }   else if (nilai >= 55){
+            ABC = "D";
+        }   else {
+            ABC = "E";
+        }
+
+        System.out.print("Nilai mu adalah : " + ABC);
+
+        scanner.close();
+    }
+}
