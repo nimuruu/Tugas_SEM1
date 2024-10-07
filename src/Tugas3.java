0ea800b20ab97d9a21b5d8aba57f774a93dd32cf
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-07 11:25:37 +0700

    Tugas Latiihan 4

--- src/Tugas3.java
+++ src/Tugas3.java
@@ -5,22 +5,36 @@ public class Tugas3 {
         double total, harga;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Total bayar :");
+        System.out.print("Total bayar : ");
         double bayar = scanner.nextInt();
+
+        //ternary 50000 ? 50000 : harga
 
         if (bayar >= 200000){
             total = bayar*12/100;
+            if (total > 50000){
+                total = 50000;
+            }
             harga = bayar - total;
+
             System.out.print("Total anda adalah : " + harga);
         }
-        else if (bayar >= 100000 && bayar <=200000){
+        else if (bayar >= 100000){
             total = bayar*10/100;
+            if (total > 15000) {
+                total = 15000;
+            }
             harga = bayar - total;
+
             System.out.print("Total anda adalah : " + harga);
         }
-        else if (bayar >= 50000 ){
+        else if (bayar >= 50000){
             total = bayar*5/100;
+            if (total > 4000) {
+                total = 4000;
+            }
             harga = bayar - total;
+
             System.out.print("Total anda adalah : " + harga);
         }
         else {
---
+++ src/Tugas4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Tugas4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double diskon, kembalian, telur, harga, jumlah;
+
+        System.out.print("Masukan jumlah beli telur (KG) : ");
+        telur = scanner.nextInt();
+
+        harga = 28000;
+        
+        jumlah = harga * telur;
+
+        if (telur >= 3) {
+            diskon = jumlah * 0.1;      
+        }
+        else {
+            diskon = jumlah;
+        }
+
+        kembalian = jumlah - diskon;
+
+        System.out.println("Total telur (KG) : " + telur);
+        System.out.println("Total harga : " + jumlah);
+        System.out.println("Diskon : " + diskon);
+        System.out.println("Uang yang harus dibayar : " + jumlah);
+        System.out.println("Uang kembalian : " + kembalian);
+
+
+        scanner.close();
+    }
+}
