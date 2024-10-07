d08273060e59e2033cf5e4296866feca2fc50c7a
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-07 08:50:13 +0700

    Tugas Latihan

---
+++ src/Hari.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Hari {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Masukan hari : ");
+        String hari = scanner.nextLine();
+        hari = hari.toLowerCase();
+
+        if (hari.equals("sabtu") || hari.equals("minggu")) {
+            System.out.print(hari + " adalah hari libur!");
+        }
+        else {
+            System.out.print(hari + " adalah hari kerja!");
+        } 
+
+        scanner.close();
+
+    }
+}
