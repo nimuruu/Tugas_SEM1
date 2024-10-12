e9492fac11f50a7edca1a45b9b7308646a74125b
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-12 16:46:41 +0700

    Latihan 6

---
+++ src/Latihan/Latiihan6.java
@@ -0,0 +1,16 @@
+package Latihan;
+
+import java.util.Scanner;
+public class Latiihan6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 4; i++) { // Loop untuk setiap baris
+            for (int j = 1; j <= i; j++) { // Loop untuk mencetak bintang di setiap baris
+                System.out.print("* "); // Mencetak bintang
+            }
+            System.out.println(); // Pindah ke baris berikutnya
+        }
+        scanner.close();
+    }
+}
