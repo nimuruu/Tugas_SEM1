cb5b4843ca943af95a672ea916ede5059ef6b632
Nimuruu <naufalmeitsamar@gmail.com>
2024-10-12 20:31:14 +0700

    Update nama

--- src/Latihan/Latihan6.java
+++ src/Latihan/Latihan6.java
@@ -1,7 +1,7 @@
 package Latihan;
 
 import java.util.Scanner;
-public class Latiihan6 {
+public class Latihan6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
