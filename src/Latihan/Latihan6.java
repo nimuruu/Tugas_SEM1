be804a3cb763cf4d2fb99461aefc12c1d7364520
Nimuruu <naufalmeitsamar@gmail.com>
2024-11-10 14:43:43 +0700

    test

--- src/Latihan/Latihan6.java
+++ src/Latihan/Latihan6.java
@@ -5,11 +5,11 @@ public class Latihan6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 4; i++) { // Loop untuk setiap baris
-            for (int j = 1; j <= i; j++) { // Loop untuk mencetak bintang di setiap baris
-                System.out.print("* "); // Mencetak bintang
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) { 
+                System.out.print("* ");
             }
-            System.out.println(); // Pindah ke baris berikutnya
+            System.out.println();
         }
         scanner.close();
     }
